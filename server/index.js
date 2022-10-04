909e798a4dcd0cf384d19c3271e5dfb8bc2fb2c0
Peter Macharia <petergichimu07@gmail.com>
2022-10-04 15:48:44 +0300

    backend code updates

--- server/index.js
+++ server/index.js
@@ -6,12 +6,13 @@ import postRoutes from './routes/posts.js'
 
 const app = express();
 
-app.use('/posts', postRoutes);
+
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use('/posts', postRoutes);
 const CONNECTION_URL =
   "mongodb+srv://petergichimu07:P12E34T56@cluster0.0gclnul.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
