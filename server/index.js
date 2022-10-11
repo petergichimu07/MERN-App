7619dd13462de8c3da9b37130ff7a3435ab04064
Peter Macharia <petergichimu07@gmail.com>
2022-10-11 13:27:14 +0300

    Backend functionality updates

--- server/index.js
+++ server/index.js
@@ -2,25 +2,25 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js'
 
 const app = express();
 
-
+dotenv.config()
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
-const CONNECTION_URL =
-  "mongodb+srv://petergichimu07:P12E34T56@cluster0.0gclnul.mongodb.net/?retryWrites=true&w=majority";
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(process.env.CONNECTION_URL)
   .then(() =>
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
   .catch((error) => console.log(error.message));
 
-// mongoose.set("useFindAndModify", false); //to ensure no warnings in the console
+
